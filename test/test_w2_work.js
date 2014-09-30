7c98042698120621ad70eafbb490125dca69999f
Adrian Lange <alange@signal.co>
2014-09-29 21:10:24 -0500

    update unit tests to use convenience factory method

--- test/test_w2_work.js
+++ test/test_w2_work.js
@@ -23,57 +23,57 @@ MW.on("run_sendVectorToWorkers", function() {
 });
 
 MW.on("run_vectorDot", function() {
-	var v = Vector.fromArray([0.0, 2.0, 4.0, 6.0, 8.0], id, nworkers);
-	var w = Vector.fromArray([1.0, 1.0, 1.0, 1.0, 1.0], id, nworkers);
+	var v = MW.newVectorFromArray([0.0, 2.0, 4.0, 6.0, 8.0]);
+	var w = MW.newVectorFromArray([1.0, 1.0, 1.0, 1.0, 1.0]);
 	v.wkDot(w, "vectorDot");
 });
 
 MW.on("run_vectorPlus", function() {
-	var v = Vector.fromArray([0.0, 1.0, 2.0, 3.0, 4.0], id, nworkers);
-	var w = Vector.fromArray([0.0, 1.0, 2.0, 3.0, 4.0], id, nworkers);
+	var v = MW.newVectorFromArray([0.0, 1.0, 2.0, 3.0, 4.0]);
+	var w = MW.newVectorFromArray([0.0, 1.0, 2.0, 3.0, 4.0]);
 	v.wkPlus(w, "vectorPlus");
 });
 
 MW.on("run_vectorMinus", function() {
-	var v = Vector.fromArray([1.0, 2.0, 3.0, 4.0, 5.0], id, nworkers);
-	var w = Vector.fromArray([0.0, 1.0, 2.0, 3.0, 4.0], id, nworkers);
+	var v = MW.newVectorFromArray([1.0, 2.0, 3.0, 4.0, 5.0]);
+	var w = MW.newVectorFromArray([0.0, 1.0, 2.0, 3.0, 4.0]);
 	v.wkMinus(w, "vectorMinus");
 });
 
 MW.on("run_vectorTimes", function() {
-	var v = Vector.fromArray([1.0, 2.0, 3.0, 4.0, 5.0], id, nworkers);
-	var w = Vector.fromArray([0.0, 1.0, 2.0, 3.0, 4.0], id, nworkers);
+	var v = MW.newVectorFromArray([1.0, 2.0, 3.0, 4.0, 5.0]);
+	var w = MW.newVectorFromArray([0.0, 1.0, 2.0, 3.0, 4.0]);
 	v.wkTimes(w, "vectorTimes");
 });
 
 MW.on("run_vectorDividedBy", function() {
-	var v = Vector.fromArray([0.0, 4.0, -8.0, 1.0, 5.0], id, nworkers);
-	var w = Vector.fromArray([1.0, 2.0, 4.0, 4.0, 5.0], id, nworkers);
+	var v = MW.newVectorFromArray([0.0, 4.0, -8.0, 1.0, 5.0]);
+	var w = MW.newVectorFromArray([1.0, 2.0, 4.0, 4.0, 5.0]);
 	v.wkDividedBy(w, "vectorDividedBy");
 });
 
 MW.on("run_vectorScale", function() {
-	var v = Vector.fromArray([1.0, 2.0, 3.0, 4.0, 5.0], id, nworkers);
+	var v = MW.newVectorFromArray([1.0, 2.0, 3.0, 4.0, 5.0]);
 	v.wkScale(2.0, "vectorScale");
 });
 
 MW.on("run_vectorApply", function() {
-	var v = Vector.fromArray([1.0, 2.0, 3.0, 4.0, 5.0], id, nworkers);
+	var v = MW.newVectorFromArray([1.0, 2.0, 3.0, 4.0, 5.0]);
 	v.wkApply(Math.sqrt, "vectorApply");
 });
 
 MW.on("run_vectorNorm", function() {
-	var v = Vector.fromArray([0.0, 1.0, 2.0, 3.0, 4.0], id, nworkers);
+	var v = MW.newVectorFromArray([0.0, 1.0, 2.0, 3.0, 4.0]);
 	v.wkNorm("vectorNorm");
 });
 
 MW.on("run_vectorSum", function() {
-	var v = Vector.fromArray([100.0, 200.0, 300.0, 400.0, 500.0], id, nworkers);
+	var v = MW.newVectorFromArray([100.0, 200.0, 300.0, 400.0, 500.0]);
 	v.wkSum("vectorSum");
 });
 
 MW.on("run_vectorTimesMatrix", function() {
-	var v = Vector.fromArray([1.0, 2.0, 3.0], id, nworkers);
-	var A = Matrix.fromArray([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]], id, nworkers);
+	var v = MW.newVectorFromArray([1.0, 2.0, 3.0]);
+	var A = MW.newMatrixFromArray([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]);
 	var w = v.wkTimesMatrix(A, "vectorTimesMatrix");
 });
