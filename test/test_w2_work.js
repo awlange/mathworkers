79915d8b5ac0ae42ecb44f4bc6d1ec542e9055a7
Adrian Lange <alange@signal.co>
2014-10-05 23:02:49 -0500

    renamed times and dividedBy, part 2

--- test/test_w2_work.js
+++ test/test_w2_work.js
@@ -56,16 +56,16 @@ MW.on("run_vectorMinus", function() {
 	v.wkMinus(w, "vectorMinus");
 });
 
-MW.on("run_vectorTimes", function() {
-	var v = Vector.fromArray([1.0, 2.0, 3.0, 4.0, 5.0]);
-	var w = Vector.fromArray([0.0, 1.0, 2.0, 3.0, 4.0]);
-	v.wkTimes(w, "vectorTimes");
+MW.on("run_vectorTimesElementwise", function() {
+	var v = MW.newVectorFromArray([1.0, 2.0, 3.0, 4.0, 5.0]);
+	var w = MW.newVectorFromArray([0.0, 1.0, 2.0, 3.0, 4.0]);
+	v.wkTimes(w, "vectorTimesElementwise");
 });
 
-MW.on("run_vectorDividedBy", function() {
-	var v = Vector.fromArray([0.0, 4.0, -8.0, 1.0, 5.0]);
-	var w = Vector.fromArray([1.0, 2.0, 4.0, 4.0, 5.0]);
-	v.wkDividedBy(w, "vectorDividedBy");
+MW.on("run_vectorDivide", function() {
+	var v = MW.newVectorFromArray([0.0, 4.0, -8.0, 1.0, 5.0]);
+	var w = MW.newVectorFromArray([1.0, 2.0, 4.0, 4.0, 5.0]);
+	v.wkDividedBy(w, "vectorDivide");
 });
 
 MW.on("run_vectorScale", function() {
@@ -112,16 +112,16 @@ MW.on("run_matrixMinus", function() {
 	A.wkMinus(B, "matrixMinus");
 });
 
-MW.on("run_matrixTimes", function() {
-	var A = Matrix.fromArray([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]);
-	var B = Matrix.fromArray([[3.0, 2.0, 1.0], [6.0, 5.0, 4.0], [9.0, 8.0, 7.0]]);
-	A.wkTimes(B, "matrixTimes");
+MW.on("run_matrixTimesElementwise", function() {
+	var A = MW.newMatrixFromArray([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]);
+	var B = MW.newMatrixFromArray([[3.0, 2.0, 1.0], [6.0, 5.0, 4.0], [9.0, 8.0, 7.0]]);
+	A.wkTimes(B, "matrixTimesElementwise");
 });
 
-MW.on("run_matrixDividedBy", function() {
-	var A = Matrix.fromArray([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]);
-	var B = Matrix.fromArray([[3.0, 2.0, 1.0], [6.0, 5.0, 4.0], [9.0, 8.0, 7.0]]);
-	A.wkDividedBy(B, "matrixDividedBy");
+MW.on("run_matrixDivide", function() {
+	var A = MW.newMatrixFromArray([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]);
+	var B = MW.newMatrixFromArray([[3.0, 2.0, 1.0], [6.0, 5.0, 4.0], [9.0, 8.0, 7.0]]);
+	A.wkDividedBy(B, "matrixDivide");
 });
 
 MW.on("run_matrixScale", function() {
