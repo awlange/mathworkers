a00af31e9666a65878444f7b14bf47ab95e35b1c
Adrian Lange <alange@signal.co>
2014-10-19 22:05:01 -0500

    basic stats tests

--- src/statistics/basic_statistics.js
+++ src/statistics/basic_statistics.js
@@ -6,47 +6,85 @@
 MW.stats = {};
 
 /**
- * Compute basic summary statistics for a generic Array or Vector
+ * Compute basic summary statistics for a generic Array, Float64Array, Vector, or Matrix
  *
  * Returns an object containing number of elements, mean, standard deviation,
- * minimum, maximum, and quartiles
+ * minimum, maximum, and quartiles. The quartiles computed here are the so-called
+ * "Tukey's Hinges".
+ *
+ * Quartiles are not reported if the data passed in contains less than 3 elements
  */
 MW.stats.summary = function(data) {
     MW.util.checkNullOrUndefined(data);
-    var arr;
+    // Copy the data to a local array so that we can sort without affecting data
+    var i;
+    var arr = [];
     if (data instanceof MW.Vector) {
-        arr = data.array;
+        for (i = 0; i < data.array.length; ++i) {
+            arr.push(data.array[i]);
+        }
+    } else if (data instanceof MW.Matrix) {
+        for (i = 0; i < data.nrows; ++i) {
+            for (var j = 0; j < data.ncols; ++j) {
+                arr.push(data.array[i][j]);
+            }
+        }
     } else if (data instanceof Array || data instanceof Float64Array) {
-        arr = data;
+        for (i = 0; i < data.length; ++i) {
+            arr.push(data[i]);
+        }
     } else {
         throw new TypeError("Invalid data type for summary(). Must be Array, Float64Array, or Vector.");
     }
 
-    var i;
+    var tmp;
+    var n = arr.length;
     var tot = 0.0;
     var amax = Math.max.apply(Math, arr);
     var amin = Math.min.apply(Math, arr);
-    for (i = 0; i < arr.length; ++i) {
+    for (i = 0; i < n; ++i) {
         tot += arr[i];
     }
-    var mean = tot / arr.length;
-    tot = 0.0;
-    for (i = 0; i < arr.length; ++i) {
-        var tmp = mean - arr[i];
-        tot += tmp * tmp;
+    var mean = tot / n;
+    var variance = 0.0;
+    for (i = 0; i < n; ++i) {
+        tmp = mean - arr[i];
+        variance += tmp * tmp;
     }
-    var stddev = Math.sqrt(tot / arr.length);
+    variance /= n;
+    var stddev = Math.sqrt(variance);
 
-    // Sort for quartiles
-    arr.sort(function(a, b){return a-b});
-    var fourth = (nRuns / 4)|0;
-    var q25 = arr[fourth - 1];
-    var q50 = arr[2*fourth - 1];
-    var q75 = arr[3*fourth - 1];
+    var q25, q50, q75;
+    if (n >= 3) {
+        // Sort for quartiles
+        arr.sort(function(a, b){return a-b});
+
+        var x, y;
+        x = getMedian(0, n-1);
+        q50 = x.median;
+        y = getMedian(x.half, n-1);
+        q75 = y.median;
+        if (x.odd) {
+            y = getMedian(0, x.half);
+            q25 = y.median;
+        } else {
+            y = getMedian(0, x.half-1);
+            q25 = y.median;
+        }
+    }
+
+    function getMedian(nfrom, nto) {
+        var m = nto - nfrom + 1;
+        var half = (m/2)|0;
+        var odd = (m % 2);
+        var median = odd ? arr[nfrom + half] : 0.5 * (arr[nfrom + half-1] + arr[nfrom + half]);
+        return {median: median, half: half, odd: odd};
+    }
 
     return {
         n: arr.length,
         mean: mean,
+        variance: variance,
         stddev: stddev,
         minimum: amin,
         maximum: amax,
